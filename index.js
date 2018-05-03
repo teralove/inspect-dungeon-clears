a24596bdb23ee6e5cf90422ebc948c9af1e31052
teralove <terascripts@gmail.com>
2018-05-03 05:45:38 -0400

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+const Command = require('command');
+
+module.exports = function InspectDungeonClears(dispatch) {
+    const command = Command(dispatch);
+    
+    const DungeonNames = {
+        9950: "HH20",
+        9935: "RKHM",
+        9970: "RMHM",
+        9710: "BP",
+        9716: "SCNM",
+        9735: "RKNM",
+        9770: "RMNM",
+        9969: "LKHM",
+        9994: "TRHM",
+        9760: "KD",
+        9769: "LKNM",
+        9794: "TRNM",
+        9055: "RG",
+        9766: "SF",
+        9860: "KC",
+        9809: "MC",
+        9808: "SA",
+        9025: "BT",
+        9026: "ACNM",
+        9727: "MCNM",
+    };
+    
+    let playerName,
+    targetName;
+    
+    dispatch.hook('S_LOGIN', 10, (event) => {
+        playerName = event.name;
+    });
+    
+    dispatch.hook('C_DUNGEON_CLEAR_COUNT_LIST', 1, (event) => {
+        targetName = event.name;
+    });
+        
+    dispatch.hook('S_DUNGEON_CLEAR_COUNT_LIST', 1, (event) => {
+        if (playerName === event.name) return;
+        
+        command.message(' ' + targetName + '\'s Dungeon Clears...');
+        for (let i = 0; i < event.dungeons.length; i++) {
+            if (DungeonNames[event.dungeons[i].id]) {
+                command.message(' ' + DungeonNames[event.dungeons[i].id] + '\t\t' + event.dungeons[i].clears);
+            }
+        }
+    });
+}
